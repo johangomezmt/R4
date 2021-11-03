87597c6a041a79ee83564b3011f78b22de78a281
johangomezmt <91511953+johangomezmt@users.noreply.github.com>
2021-11-02 22:18:06 -0500

    Delete reto3 directory

--- reto3/src/main/java/co/usa/reto3/reto3/repository/crud/AdminCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package co.usa.reto3.reto3.repository.crud;
-import org.springframework.data.repository.CrudRepository;
-import co.usa.reto3.reto3.model.Admin;
-
-public interface AdminCrudRepository extends CrudRepository<Admin,Integer>{
-
-}
--- reto3/src/main/java/co/usa/reto3/reto3/repository/crud/ReservationCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package co.usa.reto3.reto3.repository.crud;
-import org.springframework.data.repository.CrudRepository;
-import co.usa.reto3.reto3.model.Reservation;
-
-public interface ReservationCrudRepository extends CrudRepository<Reservation,Integer>{
-
-}
